9bc78549d06e16bec6770c57d50b7bddbc8135b3
Matthew-OBzera <mobzera516@gmail.com>
2016-04-07 19:53:08 -0500

    Setup
    Initial project setup with weatherxml parser added

---
+++ Project1/app/src/main/java/com/example/project1/WeatherXmlParser/ForecastPeriod.java
@@ -0,0 +1,14 @@
+package com.example.project1.WeatherXmlParser;
+
+import java.util.Date;
+
+/**
+ * Created by craig_000 on 4/26/2015.
+ */
+public class ForecastPeriod {
+    public String timeDesc;
+    public Date time;
+    public String description;
+    public double temperature;
+    public String details;
+}
